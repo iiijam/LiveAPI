7740e030abd492b0acd5727d70f03324cae40fb4
iiijam <16839488+iiijam@users.noreply.github.com>
2022-08-18 17:52:47 +0800

    fix import

--- douyu.py
+++ douyu.py
@@ -1,11 +1,9 @@
 # 获取斗鱼直播间的真实流媒体地址，默认最高画质
 # 使用 https://github.com/wbt5/real-url/issues/185 中两位大佬@wjxgzz @4bbu6j5885o3gpv6ss8找到的的CDN，在此感谢！
 import hashlib
-from operator import rshift
 import re
-from shutil import ExecError
 import time
-from turtle import title
+
 
 import execjs
 import requests
